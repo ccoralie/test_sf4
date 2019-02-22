e4873c7057e0633f67b384023183fc11a140b67f
coralie <coraliecotte.cc@gmail.com>
2019-02-22 23:53:05 +0100

    entity property

---
+++ MaSuperAgence/src/Controller/PropertyController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+
+
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class PropertyController extends AbstractController
+{
+
+    public function __construct(PropertyRepository $repository, ObjectManager $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route ("/biens", name="property.index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+
+        $this->em->flush();
+
+        return $this->render('property/index.html.twig',[
+            'current_menu' => 'properties'
+        ]);
+    }
+
+    /**
+     * @Route ("/biens/{slug}-{id}", name="property.show", requirements={"slug"="[a-z0-9\-]*"})
+     * @return Response
+     */
+    public function show($slug, $id):Response
+    {
+        $property= $this->repository->find($id);
+        return $this->render('property/show.html.twig', [
+            'property'=> $property,
+            'current_menu' => 'properties'
+        ]);
+    }
+}
